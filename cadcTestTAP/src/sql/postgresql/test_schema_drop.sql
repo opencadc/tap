42d623720b58b57b54b376709cb65b76db3ae12a
Alinga Yeung <Alinga.Yeung@nrc-cnrc.gc.ca>
2016-05-24 16:53:59 -0700

    Story 1906. Deleted irrelevant file plane.xml. Added a permissions file to grant table accesses.

--- cadcTestTAP/src/sql/postgresql/test_schema_drop.sql
+++
@@ -1,9 +0,0 @@
-
--- drop all TEST tables in the correct order to not violate
--- foreign key constraints
-
-drop table TESt_SCHEMA.key_columns;
-drop table TEST_SCHEMA.keys;
-drop table TEST_SCHEMA.columns;
-drop table TEST_SCHEMA.tables;
-drop table TEST_SCHEMA.schemas;
