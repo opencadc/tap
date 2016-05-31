c1a3064dd188aacf604daeab434276a055429d53
Patrick Dowler <patrick.dowler@nrc-cnrc.gc.ca>
2016-05-31 14:13:48 -0700

    merged s1881 and fixed bugs with wrong arraysize in votable output

--- cadcTAP/src/ca/nrc/cadc/tap/DefaultTableWriter.java
+++ cadcTAP/src/ca/nrc/cadc/tap/DefaultTableWriter.java
@@ -1,3 +1,72 @@
+/*
+************************************************************************
+*******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
+**************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
+*
+*  (c) 2009.                            (c) 2009.
+*  Government of Canada                 Gouvernement du Canada
+*  National Research Council            Conseil national de recherches
+*  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
+*  All rights reserved                  Tous droits réservés
+*                                       
+*  NRC disclaims any warranties,        Le CNRC dénie toute garantie
+*  expressed, implied, or               énoncée, implicite ou légale,
+*  statutory, of any kind with          de quelque nature que ce
+*  respect to the software,             soit, concernant le logiciel,
+*  including without limitation         y compris sans restriction
+*  any warranty of merchantability      toute garantie de valeur
+*  or fitness for a particular          marchande ou de pertinence
+*  purpose. NRC shall not be            pour un usage particulier.
+*  liable in any event for any          Le CNRC ne pourra en aucun cas
+*  damages, whether direct or           être tenu responsable de tout
+*  indirect, special or general,        dommage, direct ou indirect,
+*  consequential or incidental,         particulier ou général,
+*  arising from the use of the          accessoire ou fortuit, résultant
+*  software.  Neither the name          de l'utilisation du logiciel. Ni
+*  of the National Research             le nom du Conseil National de
+*  Council of Canada nor the            Recherches du Canada ni les noms
+*  names of its contributors may        de ses  participants ne peuvent
+*  be used to endorse or promote        être utilisés pour approuver ou
+*  products derived from this           promouvoir les produits dérivés
+*  software without specific prior      de ce logiciel sans autorisation
+*  written permission.                  préalable et particulière
+*                                       par écrit.
+*                                       
+*  This file is part of the             Ce fichier fait partie du projet
+*  OpenCADC project.                    OpenCADC.
+*                                       
+*  OpenCADC is free software:           OpenCADC est un logiciel libre ;
+*  you can redistribute it and/or       vous pouvez le redistribuer ou le
+*  modify it under the terms of         modifier suivant les termes de
+*  the GNU Affero General Public        la “GNU Affero General Public
+*  License as published by the          License” telle que publiée
+*  Free Software Foundation,            par la Free Software Foundation
+*  either version 3 of the              : soit la version 3 de cette
+*  License, or (at your option)         licence, soit (à votre gré)
+*  any later version.                   toute version ultérieure.
+*                                       
+*  OpenCADC is distributed in the       OpenCADC est distribué
+*  hope that it will be useful,         dans l’espoir qu’il vous
+*  but WITHOUT ANY WARRANTY;            sera utile, mais SANS AUCUNE
+*  without even the implied             GARANTIE : sans même la garantie
+*  warranty of MERCHANTABILITY          implicite de COMMERCIALISABILITÉ
+*  or FITNESS FOR A PARTICULAR          ni d’ADÉQUATION À UN OBJECTIF
+*  PURPOSE.  See the GNU Affero         PARTICULIER. Consultez la Licence
+*  General Public License for           Générale Publique GNU Affero
+*  more details.                        pour plus de détails.
+*                                       
+*  You should have received             Vous devriez avoir reçu une
+*  a copy of the GNU Affero             copie de la Licence Générale
+*  General Public License along         Publique GNU Affero avec
+*  with OpenCADC.  If not, see          OpenCADC ; si ce n’est
+*  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
+*                                       <http://www.gnu.org/licenses/>.
+*
+*  $Revision: 4 $
+*
+************************************************************************
+*/
+
 package ca.nrc.cadc.tap;
 
 import ca.nrc.cadc.auth.AuthMethod;
@@ -560,13 +629,20 @@ public class DefaultTableWriter implements TableWriter
         if (datatype == null)
             return;
 
-        if (datatype.equals("adql:VARBINARY"))
+        if (datatype.equals("adql:BINARY"))
+        {
+            if (size != null)
+                field.setArraysize(size);
+        }
+        else if (datatype.equals("adql:VARBINARY"))
         {
             field.setVariableSize(true);
+            if (size != null)
+                field.setArraysize(size);
         }
         else if (datatype.equals("adql:CHAR"))
         {
-            field.setVariableSize(true);
+            //field.setVariableSize(true);
             if (size != null)
                 field.setArraysize(size);
         }
@@ -576,25 +652,16 @@ public class DefaultTableWriter implements TableWriter
             if (size != null)
                 field.setArraysize(size);
         }
-        else if (datatype.equals("adql:BINARY"))
-        {
-            if (size == null)
-                field.setVariableSize(true);
-            else
-                field.setArraysize(size);
-        }
         else if (datatype.equals("adql:BLOB"))
         {
-            if (size == null)
-                field.setVariableSize(true);
-            else
+            field.setVariableSize(true);
+            if (size != null)
                 field.setArraysize(size);
         }
         else if (datatype.equals("adql:CLOB"))
         {
-            if (size == null)
-                field.setVariableSize(true);
-            else
+            field.setVariableSize(true);
+            if (size != null)
                 field.setArraysize(size);
         }
         else if (datatype.equals("adql:TIMESTAMP"))
@@ -623,9 +690,7 @@ public class DefaultTableWriter implements TableWriter
         {
             field.setVariableSize(true);
         }
-        // here we support votable datatypes used directly in the tap_schema,
-        // which are normally only needed if the DB has arrays of primitive types
-        // as adql types cover all the other scenarios
+        // VOTable types
         else if (datatype.equals("votable:double"))
         {
             if (size != null)
@@ -665,8 +730,8 @@ public class DefaultTableWriter implements TableWriter
         {
             field.setVariableSize(true);
         }
-        
-        if (datatype.equals("point"))
+        // DALI-1.1 xtypes
+        else if (datatype.equals("point"))
         {
             field.setArraysize(2);
         }
