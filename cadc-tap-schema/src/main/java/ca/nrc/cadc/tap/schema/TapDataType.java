198bcdf008499abdb79c7c39fac7b87f11bbf710
Patrick Dowler <patrick.dowler@nrc-cnrc.gc.ca>
2017-02-24 10:25:01 -0800

    add new files for previous commit

---
+++ cadc-tap-schema/src/main/java/ca/nrc/cadc/tap/schema/TapDataType.java
@@ -0,0 +1,155 @@
+/*
+************************************************************************
+*******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
+**************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
+*
+*  (c) 2016.                            (c) 2016.
+*  Government of Canada                 Gouvernement du Canada
+*  National Research Council            Conseil national de recherches
+*  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
+*  All rights reserved                  Tous droits réservés
+*
+*  NRC disclaims any warranties,        Le CNRC dénie toute garantie
+*  expressed, implied, or               énoncée, implicite ou légale,
+*  statutory, of any kind with          de quelque nature que ce
+*  respect to the software,             soit, concernant le logiciel,
+*  including without limitation         y compris sans restriction
+*  any warranty of merchantability      toute garantie de valeur
+*  or fitness for a particular          marchande ou de pertinence
+*  purpose. NRC shall not be            pour un usage particulier.
+*  liable in any event for any          Le CNRC ne pourra en aucun cas
+*  damages, whether direct or           être tenu responsable de tout
+*  indirect, special or general,        dommage, direct ou indirect,
+*  consequential or incidental,         particulier ou général,
+*  arising from the use of the          accessoire ou fortuit, résultant
+*  software.  Neither the name          de l'utilisation du logiciel. Ni
+*  of the National Research             le nom du Conseil National de
+*  Council of Canada nor the            Recherches du Canada ni les noms
+*  names of its contributors may        de ses  participants ne peuvent
+*  be used to endorse or promote        être utilisés pour approuver ou
+*  products derived from this           promouvoir les produits dérivés
+*  software without specific prior      de ce logiciel sans autorisation
+*  written permission.                  préalable et particulière
+*                                       par écrit.
+*
+*  This file is part of the             Ce fichier fait partie du projet
+*  OpenCADC project.                    OpenCADC.
+*
+*  OpenCADC is free software:           OpenCADC est un logiciel libre ;
+*  you can redistribute it and/or       vous pouvez le redistribuer ou le
+*  modify it under the terms of         modifier suivant les termes de
+*  the GNU Affero General Public        la “GNU Affero General Public
+*  License as published by the          License” telle que publiée
+*  Free Software Foundation,            par la Free Software Foundation
+*  either version 3 of the              : soit la version 3 de cette
+*  License, or (at your option)         licence, soit (à votre gré)
+*  any later version.                   toute version ultérieure.
+*
+*  OpenCADC is distributed in the       OpenCADC est distribué
+*  hope that it will be useful,         dans l’espoir qu’il vous
+*  but WITHOUT ANY WARRANTY;            sera utile, mais SANS AUCUNE
+*  without even the implied             GARANTIE : sans même la garantie
+*  warranty of MERCHANTABILITY          implicite de COMMERCIALISABILITÉ
+*  or FITNESS FOR A PARTICULAR          ni d’ADÉQUATION À UN OBJECTIF
+*  PURPOSE.  See the GNU Affero         PARTICULIER. Consultez la Licence
+*  General Public License for           Générale Publique GNU Affero
+*  more details.                        pour plus de détails.
+*
+*  You should have received             Vous devriez avoir reçu une
+*  a copy of the GNU Affero             copie de la Licence Générale
+*  General Public License along         Publique GNU Affero avec
+*  with OpenCADC.  If not, see          OpenCADC ; si ce n’est
+*  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
+*                                       <http://www.gnu.org/licenses/>.
+*
+************************************************************************
+*/
+
+package ca.nrc.cadc.tap.schema;
+
+
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author pdowler
+ */
+public class TapDataType 
+{
+    private static final Logger log = Logger.getLogger(TapDataType.class);
+
+    private String datatype;
+    public Integer arraysize;
+    public Boolean varSize;
+    public String xtype;
+    
+    public TapDataType(String datatype) 
+    {
+        TapSchema.assertNotNull(TapDataType.class, "datatype", datatype);
+        this.datatype = datatype;
+        this.varSize = false;
+    }
+    
+    public TapDataType(String datatype, Integer arraysize, Boolean varSize, String xtype)
+    {
+        this(datatype);
+        this.arraysize = arraysize;
+        this.varSize = varSize;
+        this.xtype = xtype;
+    }
+
+    public String getDatatype()
+    {
+        return datatype;
+    }
+    
+    @Override
+    public String toString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("TapDataType[").append(datatype).append(",");
+        sb.append(arraysize).append(",");
+        sb.append(varSize).append(",");
+        sb.append(xtype).append("]");
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (obj != null && obj instanceof TapDataType)
+        {
+            return (this.toString().equals(obj.toString()));
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return toString().hashCode();
+    }
+    
+    
+    
+    public static final TapDataType FUNCTION_ARG = new TapDataType("function-arg", null, null, null);
+    
+    // VOTable primitive types
+    public static final TapDataType SHORT = new TapDataType("short");
+    public static final TapDataType INTEGER = new TapDataType("int");
+    public static final TapDataType LONG = new TapDataType("long");
+    public static final TapDataType FLOAT = new TapDataType("float");
+    public static final TapDataType DOUBLE = new TapDataType("double");
+    public static final TapDataType CHAR = new TapDataType("char");
+    
+    // DALI types
+    public static final TapDataType TIMESTAMP = new TapDataType("char", null, true, "timestamp");
+    public static final TapDataType INTERVAL = new TapDataType("double", 2, false, "interval");
+    public static final TapDataType POINT = new TapDataType("double", 2, false, "point");
+    public static final TapDataType CIRCLE = new TapDataType("double", 3, false, "circle");
+    public static final TapDataType POLYGON = new TapDataType("double", null, true, "polygon");
+    
+    // ADQL types
+    public static final TapDataType BLOB = new TapDataType("byte", null, true, "blob");
+    public static final TapDataType CLOB = new TapDataType("char", null, true, "clob");
+}
---
+++ cadc-tap-server/src/main/java/ca/nrc/cadc/tap/TapSelectItem.java
@@ -0,0 +1,156 @@
+/*
+************************************************************************
+*******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
+**************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
+*
+*  (c) 2016.                            (c) 2016.
+*  Government of Canada                 Gouvernement du Canada
+*  National Research Council            Conseil national de recherches
+*  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
+*  All rights reserved                  Tous droits réservés
+*
+*  NRC disclaims any warranties,        Le CNRC dénie toute garantie
+*  expressed, implied, or               énoncée, implicite ou légale,
+*  statutory, of any kind with          de quelque nature que ce
+*  respect to the software,             soit, concernant le logiciel,
+*  including without limitation         y compris sans restriction
+*  any warranty of merchantability      toute garantie de valeur
+*  or fitness for a particular          marchande ou de pertinence
+*  purpose. NRC shall not be            pour un usage particulier.
+*  liable in any event for any          Le CNRC ne pourra en aucun cas
+*  damages, whether direct or           être tenu responsable de tout
+*  indirect, special or general,        dommage, direct ou indirect,
+*  consequential or incidental,         particulier ou général,
+*  arising from the use of the          accessoire ou fortuit, résultant
+*  software.  Neither the name          de l'utilisation du logiciel. Ni
+*  of the National Research             le nom du Conseil National de
+*  Council of Canada nor the            Recherches du Canada ni les noms
+*  names of its contributors may        de ses  participants ne peuvent
+*  be used to endorse or promote        être utilisés pour approuver ou
+*  products derived from this           promouvoir les produits dérivés
+*  software without specific prior      de ce logiciel sans autorisation
+*  written permission.                  préalable et particulière
+*                                       par écrit.
+*
+*  This file is part of the             Ce fichier fait partie du projet
+*  OpenCADC project.                    OpenCADC.
+*
+*  OpenCADC is free software:           OpenCADC est un logiciel libre ;
+*  you can redistribute it and/or       vous pouvez le redistribuer ou le
+*  modify it under the terms of         modifier suivant les termes de
+*  the GNU Affero General Public        la “GNU Affero General Public
+*  License as published by the          License” telle que publiée
+*  Free Software Foundation,            par la Free Software Foundation
+*  either version 3 of the              : soit la version 3 de cette
+*  License, or (at your option)         licence, soit (à votre gré)
+*  any later version.                   toute version ultérieure.
+*
+*  OpenCADC is distributed in the       OpenCADC est distribué
+*  hope that it will be useful,         dans l’espoir qu’il vous
+*  but WITHOUT ANY WARRANTY;            sera utile, mais SANS AUCUNE
+*  without even the implied             GARANTIE : sans même la garantie
+*  warranty of MERCHANTABILITY          implicite de COMMERCIALISABILITÉ
+*  or FITNESS FOR A PARTICULAR          ni d’ADÉQUATION À UN OBJECTIF
+*  PURPOSE.  See the GNU Affero         PARTICULIER. Consultez la Licence
+*  General Public License for           Générale Publique GNU Affero
+*  more details.                        pour plus de détails.
+*
+*  You should have received             Vous devriez avoir reçu une
+*  a copy of the GNU Affero             copie de la Licence Générale
+*  General Public License along         Publique GNU Affero avec
+*  with OpenCADC.  If not, see          OpenCADC ; si ce n’est
+*  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
+*                                       <http://www.gnu.org/licenses/>.
+*
+************************************************************************
+*/
+
+package ca.nrc.cadc.tap;
+
+import ca.nrc.cadc.tap.schema.ColumnDesc;
+import ca.nrc.cadc.tap.schema.TapDataType;
+import ca.nrc.cadc.tap.schema.TapSchema;
+
+/**
+ * Local replacement for ParamDesc in cadc-tap-schema library.
+ * 
+ * @author pdowler
+ */
+public class TapSelectItem
+{
+    private String name;
+    private TapDataType datatype;
+
+    private String columnName;
+    public String tableName;
+
+    public String description;
+    public String utype;
+    public String ucd;
+    public String unit;
+    public boolean principal;
+    public boolean indexed;
+    public boolean std;
+    public String id;    
+    
+    /**
+     * A normal column with an alternate name (alias). 
+     * All metadata is copied from the specified column descriptor.
+     * 
+     * @param name
+     * @param column 
+     */
+    public TapSelectItem(String name, ColumnDesc column) 
+    { 
+        this(name, column.getDatatype());
+        this.columnName = column.getColumnName();
+        this.tableName = column.getTableName();
+        this.description = column.description;
+        this.id = column.id;
+        this.indexed = column.indexed;
+        this.principal = column.principal;
+        this.std = column.std;
+        this.ucd = column.ucd;
+        this.unit = column.unit;
+        this.utype = column.utype;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public TapDataType getDatatype()
+    {
+        return datatype;
+    }
+
+    /**
+     * Original column name if the selected item was a column.
+     * @return 
+     */
+    public String getColumnName()
+    {
+        return columnName;
+    }
+    
+    
+    
+    /**
+     * A new column created by some sort of expression. This could be a function call, algebraic 
+     * expression, case statement, etc. The calling code must set any additional column
+     * metadata.
+     * 
+     * @param name
+     * @param datatype
+     * @param arraysize
+     * @param varSize
+     */
+    public TapSelectItem(String name, TapDataType datatype)
+    {
+        TapSchema.assertNotNull(TapSelectItem.class, "name", name);
+        TapSchema.assertNotNull(TapSelectItem.class, "datatype", datatype);
+        this.name = name;
+        this.datatype = datatype;
+    }
+}
