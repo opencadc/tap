5cb36c37f5f311485967a31ea2c680e393f27042
Jeff Burke <Jeff.Burke@nrc-cnrc.gc.ca>
2019-10-31 08:56:55 -0700

    s2526 - increment build, update for ServletLogInfo constructor

--- cadc-tap-schema/src/main/java/ca/nrc/cadc/vosi/TableServlet.java
+++ cadc-tap-schema/src/main/java/ca/nrc/cadc/vosi/TableServlet.java
@@ -127,7 +127,8 @@ public class TableServlet extends HttpServlet
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException
     {
-        WebServiceLogInfo logInfo = new ServletLogInfo(request, this.getClass());
+        WebServiceLogInfo logInfo = new ServletLogInfo(request);
+        logInfo.setClass(this.getClass());
         long start = System.currentTimeMillis();
         
         try
