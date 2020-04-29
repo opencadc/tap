f14c827af32485c60788f68e3d3828ed77e698ab
Dustin Jenkins <djenkins.cadc@gmail.com>
2020-04-29 12:30:30 -0700

    Remove remove override.

--- cadc-tap/src/main/java/org/opencadc/tap/TsvIterator.java
+++ cadc-tap/src/main/java/org/opencadc/tap/TsvIterator.java
@@ -106,31 +106,6 @@ class TsvIterator<E> implements ResourceIterator<E> {
     }
 
     /**
-     * Removes from the underlying collection the last element returned
-     * by this iterator (optional operation).  This method can be called
-     * only once per call to {@link #next}.
-     *
-     * <p>The behavior of an iterator is unspecified if the underlying collection
-     * is modified while the iteration is in progress in any way other than by
-     * calling this method, unless an overriding class has specified a
-     * concurrent modification policy.
-     *
-     * <p>The behavior of an iterator is unspecified if this method is called
-     * after a call to the {@link #forEachRemaining forEachRemaining} method.
-     *
-     * @throws UnsupportedOperationException if the {@code remove}
-     *                                       operation is not supported by this iterator
-     * @throws IllegalStateException         if the {@code next} method has not
-     *                                       yet been called, or the {@code remove} method has already
-     *                                       been called after the last call to the {@code next}
-     *                                       method
-     */
-    @Override
-    public void remove() {
-        asciiTableData.remove();
-    }
-
-    /**
      * Closes this stream and releases any system resources associated
      * with it. If the stream is already closed then invoking this
      * method has no effect.
