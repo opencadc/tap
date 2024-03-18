c4342e8e9a10d47ed8c73c17206a52c899089ef9
Patrick Dowler <pdowler.cadc@gmail.com>
2024-03-18 09:58:21 -0700

    cadc-adql-1.1.14

--- cadc-adql/src/main/java/ca/nrc/cadc/tap/AdqlQuery.java
+++ cadc-adql/src/main/java/ca/nrc/cadc/tap/AdqlQuery.java
@@ -3,7 +3,7 @@
 *******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
 **************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
 *
-*  (c) 2017.                            (c) 2017.
+*  (c) 2024.                            (c) 2024.
 *  Government of Canada                 Gouvernement du Canada
 *  National Research Council            Conseil national de recherches
 *  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
@@ -139,7 +139,7 @@ public class AdqlQuery extends AbstractTapQuery
         super();
     }
     
-    public void setDeparserImpl(Class<? extends BaseExpressionDeParser> deparserClass) {
+    public final void setDeparserImpl(Class<? extends BaseExpressionDeParser> deparserClass) {
         this.deparserClass = deparserClass;
     }
     
