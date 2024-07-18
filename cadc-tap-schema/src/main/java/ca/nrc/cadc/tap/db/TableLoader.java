3ccfcbf749e93f7f7f9b573a076f376a4e54555a
Patrick Dowler <pdowler.cadc@gmail.com>
2024-07-18 11:50:11 -0700

    fix bad import

--- cadc-tap-schema/src/main/java/ca/nrc/cadc/tap/db/TableLoader.java
+++ cadc-tap-schema/src/main/java/ca/nrc/cadc/tap/db/TableLoader.java
@@ -3,7 +3,7 @@
 *******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
 **************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
 *
-*  (c) 2018.                            (c) 2018.
+*  (c) 2024.                            (c) 2024.
 *  Government of Canada                 Gouvernement du Canada
 *  National Research Council            Conseil national de recherches
 *  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
@@ -67,7 +67,6 @@
 
 package ca.nrc.cadc.tap.db;
 
-import static com.sun.tools.javac.jvm.ByteCodes.ret;
 import ca.nrc.cadc.dali.Circle;
 import ca.nrc.cadc.dali.DoubleInterval;
 import ca.nrc.cadc.dali.Interval;
