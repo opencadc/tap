712812048c73de0629928c35545d6543e4085445
Patrick Dowler <pdowler.cadc@gmail.com>
2025-03-27 16:33:48 -0700

    retroactively fix the tap_schema-1.1.x to 1.2.0 upgrade adding to tap_schema.columns was missing

--- cadc-tap-schema/src/main/resources/postgresql/tap_schema.upgrade-1.2.0.sql
+++ cadc-tap-schema/src/main/resources/postgresql/tap_schema.upgrade-1.2.0.sql
@@ -14,3 +14,11 @@ alter table tap_schema.tables11
     add column read_anon        integer,
     add column read_only_group  varchar(128),
     add column read_write_group varchar(128);
+
+-- this was missing from here when this was the current version, 
+-- but it was in the 1.2.0 create script
+alter table tap_schema.columns11
+    add column owner_id         varchar(32),
+    add column read_anon        integer,
+    add column read_only_group  varchar(128),
+    add column read_write_group varchar(128);
