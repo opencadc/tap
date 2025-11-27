cd46f74f1b3b02ebb97668469ae5c1c32a7d5a72
Patrick Dowler <pdowler.cadc@gmail.com>
2025-11-27 10:26:22 -0800

    cadc-tap-schema: tidy up sql files and tap_schema self description

--- cadc-tap-schema/src/main/resources/postgresql/tap_schema_self11.sql
+++ cadc-tap-schema/src/main/resources/postgresql/tap_schema_self11.sql
@@ -45,44 +45,43 @@ insert into tap_schema.tables11 (schema_name,table_name,table_type,description,u
 ( 'tap_schema', 'tap_schema.key_columns', 'table', 'description of foreign key columns in this tableset', NULL, 5)
 ;
 
-insert into tap_schema.columns11 (table_name,column_name,description,utype,ucd,unit,datatype,arraysize,xtype,principal,indexed,std, column_index) values
-( 'tap_schema.schemas', 'schema_name', 'schema name for reference to tap_schema.schemas', NULL, NULL, NULL, 'char', '64*', NULL, 1,0,1,1 ),
-( 'tap_schema.schemas', 'utype', 'lists the utypes of schemas in the tableset',           NULL, NULL, NULL, 'char', '512*', NULL, 1,0,1,2 ),
-( 'tap_schema.schemas', 'description', 'describes schemas in the tableset',               NULL, NULL, NULL, 'char', '512*', NULL, 1,0,1,3 ),
-( 'tap_schema.schemas', 'schema_index', 'recommended sort order when listing schemas',    NULL, NULL, NULL, 'int', NULL, NULL, 1,0,1,4 ),
+insert into tap_schema.columns11 (table_name,column_name,description,utype,ucd,unit,datatype,arraysize,xtype, principal,indexed,std, column_index) values
+( 'tap_schema.schemas', 'schema_name', 'schema name for reference to tap_schema.schemas', NULL, NULL, NULL, 'char', '64*',  NULL, 1,1,1, 1),
+( 'tap_schema.schemas', 'utype', 'lists the utypes of schemas in the tableset',           NULL, NULL, NULL, 'char', '512*', NULL, 1,0,1, 2),
+( 'tap_schema.schemas', 'description', 'describes schemas in the tableset',               NULL, NULL, NULL, 'char', '512*', NULL, 1,0,1, 3),
+( 'tap_schema.schemas', 'schema_index', 'recommended sort order when listing schemas',    NULL, NULL, NULL, 'int',  NULL,   NULL, 1,0,1, 4),
 
-( 'tap_schema.tables', 'schema_name', 'the schema this table belongs to',                 NULL, NULL, NULL, 'char', '512*', NULL, 1,0,1,1 ),
-( 'tap_schema.tables', 'table_name', 'the fully qualified table name',                    NULL, NULL, NULL, 'char', '64*', NULL, 1,0,1,2 ),
-( 'tap_schema.tables', 'table_type', 'one of: table view',                                NULL, NULL, NULL, 'char', '8*', NULL, 1,0,1,3 ),
-( 'tap_schema.tables', 'utype', 'lists the utype of tables in the tableset',              NULL, NULL, NULL, 'char', '512*', NULL, 1,0,1,4 ),
-( 'tap_schema.tables', 'description', 'describes tables in the tableset',                 NULL, NULL, NULL, 'char', '512*', NULL, 1,0,1,5 ),
-( 'tap_schema.tables', 'table_index', 'recommended sort order when listing tables',       NULL, NULL, NULL, 'int', NULL, NULL, 1,0,1,6 ),
+( 'tap_schema.tables', 'schema_name', 'the schema this table belongs to',                 NULL, NULL, NULL, 'char', '512*', NULL, 1,0,1, 1),
+( 'tap_schema.tables', 'table_name', 'the fully qualified table name',                    NULL, NULL, NULL, 'char', '64*',  NULL, 1,1,1, 2),
+( 'tap_schema.tables', 'table_type', 'one of: table view',                                NULL, NULL, NULL, 'char', '8*',   NULL, 1,0,1, 3),
+( 'tap_schema.tables', 'utype', 'lists the utype of tables in the tableset',              NULL, NULL, NULL, 'char', '512*', NULL, 1,0,1, 4),
+( 'tap_schema.tables', 'description', 'describes tables in the tableset',                 NULL, NULL, NULL, 'char', '512*', NULL, 1,0,1, 5),
+( 'tap_schema.tables', 'table_index', 'recommended sort order when listing tables',       NULL, NULL, NULL, 'int',  NULL,   NULL, 1,0,1, 6),
 
-( 'tap_schema.columns', 'table_name', 'the table this column belongs to',                 NULL, NULL, NULL, 'char', '64*', NULL, 1,0,1,1 ),
-( 'tap_schema.columns', 'column_name', 'the column name',                                 NULL, NULL, NULL, 'char', '64*', NULL, 1,0,1,2 ),
-( 'tap_schema.columns', 'utype', 'lists the utypes of columns in the tableset',           NULL, NULL, NULL, 'char', '512*', NULL, 1,0,1,3 ),
-( 'tap_schema.columns', 'ucd', 'lists the UCDs of columns in the tableset',               NULL, NULL, NULL, 'char', '64*', NULL, 1,0,1,4 ),
-( 'tap_schema.columns', 'unit', 'lists the unit used for column values in the tableset',  NULL, NULL, NULL, 'char', '64*', NULL, 1,0,1,5 ),
-( 'tap_schema.columns', 'description', 'describes the columns in the tableset',           NULL, NULL, NULL, 'char', '512*', NULL, 1,0,1,6 ),
-( 'tap_schema.columns', 'datatype', 'lists the ADQL datatype of columns in the tableset', NULL, NULL, NULL, 'char', '64*', NULL, 1,0,1,7 ),
-( 'tap_schema.columns', 'arraysize', 'lists the size of variable-length columns in the tableset', NULL, NULL, NULL, 'char', '16*', NULL, 1,0,1,8 ),
-( 'tap_schema.columns', 'xtype', 'a DALI or custom extended type annotation',             NULL, NULL, NULL, 'char', '64*', NULL, 1,0,1,7 ),
+( 'tap_schema.columns', 'table_name', 'the table this column belongs to',                 NULL, NULL, NULL, 'char', '64*',  NULL, 1,1,1, 1),
+( 'tap_schema.columns', 'column_name', 'the column name',                                 NULL, NULL, NULL, 'char', '64*',  NULL, 1,1,1, 2),
+( 'tap_schema.columns', 'utype', 'lists the utypes of columns in the tableset',           NULL, NULL, NULL, 'char', '512*', NULL, 1,0,1, 3),
+( 'tap_schema.columns', 'ucd', 'lists the UCDs of columns in the tableset',               NULL, NULL, NULL, 'char', '64*',  NULL, 1,0,1, 4),
+( 'tap_schema.columns', 'unit', 'lists the unit used for column values in the tableset',  NULL, NULL, NULL, 'char', '64*',  NULL, 1,0,1, 5),
+( 'tap_schema.columns', 'description', 'describes the columns in the tableset',           NULL, NULL, NULL, 'char', '512*', NULL, 1,0,1, 6),
+( 'tap_schema.columns', 'datatype', 'base datatype of columns in the tableset',           NULL, NULL, NULL, 'char', '64*',  NULL, 1,0,1, 7),
+( 'tap_schema.columns', 'arraysize', 'size of variable-length columns in the tableset',   NULL, NULL, NULL, 'char', '16*',  NULL, 1,0,1, 8),
+( 'tap_schema.columns', 'xtype', 'a DALI or custom extended type annotation',             NULL, NULL, NULL, 'char', '64*',  NULL, 1,0,1, 9),
+( 'tap_schema.columns', '"size"', 'deprecated: use arraysize',                            NULL, NULL, NULL, 'int',  NULL,   NULL, 1,0,1, 10),
+( 'tap_schema.columns', 'principal', 'a principal column; 1 means true, 0 means false',   NULL, NULL, NULL, 'int',  NULL,   NULL, 1,0,1, 11),
+( 'tap_schema.columns', 'indexed', 'an indexed column; 1 means true, 0 means false',      NULL, NULL, NULL, 'int',  NULL,   NULL, 1,0,1, 12),
+( 'tap_schema.columns', 'std', 'a standard column; 1 means true, 0 means false',          NULL, NULL, NULL, 'int',  NULL,   NULL, 1,0,1, 13),
+( 'tap_schema.columns', 'column_index', 'recommended sort order for columns of a table',  NULL, NULL, NULL, 'int',  NULL,   NULL, 1,0,1, 14),
 
-( 'tap_schema.columns', '"size"', 'deprecated: use arraysize', NULL, NULL, NULL, 'int', NULL, NULL, 1,0,1,9 ),
-( 'tap_schema.columns', 'principal', 'a principal column; 1 means true, 0 means false',      NULL, NULL, NULL, 'int', NULL, NULL, 1,0,1,10 ),
-( 'tap_schema.columns', 'indexed', 'an indexed column; 1 means true, 0 means false',         NULL, NULL, NULL, 'int', NULL, NULL, 1,0,1,11 ),
-( 'tap_schema.columns', 'std', 'a standard column; 1 means true, 0 means false',             NULL, NULL, NULL, 'int', NULL, NULL, 1,0,1,12 ),
-( 'tap_schema.columns', 'column_index', 'recommended sort order when listing columns of a table',  NULL, NULL, NULL, 'int', NULL, NULL, 1,0,1,13 ),
+( 'tap_schema.keys', 'key_id', 'unique key to join to tap_schema.key_columns',            NULL, NULL, NULL, 'char', '64*',  NULL, 1,0,1, 1),
+( 'tap_schema.keys', 'from_table', 'the table with the foreign key',                      NULL, NULL, NULL, 'char', '64*',  NULL, 1,0,1, 2),
+( 'tap_schema.keys', 'target_table', 'the table with the primary key',                    NULL, NULL, NULL, 'char', '64*',  NULL, 1,0,1, 3),
+( 'tap_schema.keys', 'utype', 'lists the utype of keys in the tableset',                  NULL, NULL, NULL, 'char', '512*', NULL, 1,0,1, 4),
+( 'tap_schema.keys', 'description', 'describes keys in the tableset',                     NULL, NULL, NULL, 'char', '512*', NULL, 1,0,1, 5),
 
-( 'tap_schema.keys', 'key_id', 'unique key to join to tap_schema.key_columns',            NULL, NULL, NULL, 'char', '64*', NULL, 1,0,1,1 ),
-( 'tap_schema.keys', 'from_table', 'the table with the foreign key',                      NULL, NULL, NULL, 'char', '64*', NULL, 1,0,1,2 ),
-( 'tap_schema.keys', 'target_table', 'the table with the primary key',                    NULL, NULL, NULL, 'char', '64*', NULL, 1,0,1,3 ),
-( 'tap_schema.keys', 'utype', 'lists the utype of keys in the tableset',              NULL, NULL, NULL, 'char', '512*', NULL, 1,0,1,4 ),
-( 'tap_schema.keys', 'description', 'describes keys in the tableset',                 NULL, NULL, NULL, 'char', '512*', NULL, 1,0,1,5 ),
-
-( 'tap_schema.key_columns', 'key_id', 'key to join to tap_schema.keys',                   NULL, NULL, NULL, 'char', '64*', NULL, 1,0,1,1 ),
-( 'tap_schema.key_columns', 'from_column', 'column in the from_table',                    NULL, NULL, NULL, 'char', '64*', NULL, 1,0,1,2 ),
-( 'tap_schema.key_columns', 'target_column', 'column in the target_table',                NULL, NULL, NULL, 'char', '64*', NULL, 1,0,1,3 )
+( 'tap_schema.key_columns', 'key_id', 'key to join to tap_schema.keys',                   NULL, NULL, NULL, 'char', '64*',  NULL, 1,0,1, 1),
+( 'tap_schema.key_columns', 'from_column', 'column in the from_table',                    NULL, NULL, NULL, 'char', '64*',  NULL, 1,0,1, 2),
+( 'tap_schema.key_columns', 'target_column', 'column in the target_table',                NULL, NULL, NULL, 'char', '64*',  NULL, 1,0,1, 3)
 ;
 
 insert into tap_schema.keys11 (key_id,from_table,target_table) values
